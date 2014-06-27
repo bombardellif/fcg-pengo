c7d9f07a82f93e36052934c03d5c0bf7a28320e8
Fernando Bombardelli <bombardelli.f@gmail.com>
2014-06-27 00:38:36 -0300

    Developing render codes

--- include/Scenario.h
+++ include/Scenario.h
@@ -3,14 +3,18 @@
 
 #include <string>
 
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "bitmap.h"
 #include "Modelable.h"
 
 #define SCENARIO_MAP_SIZE 15
 #define SCENARIO_MAP_FILENAME "map.bmp"
+#define SCENARIO_TEXTURE_FLOOR_FILENAME "groundTexture.bmp"
 #define SCENARIO_MAP_COLOR_IMBLOCK 0x000000FF
 #define SCENARIO_MAP_COLOR_BLOCK 0x0000FF00
-#define SCENARIO_IMBLOCK_FILENAME "al.obj"
-#define SCENARIO_BLOCK_FILENAME "al.obj"
+#define SCENARIO_IMBLOCK_FILENAME "penguin.obj"
+#define SCENARIO_BLOCK_FILENAME "penguin.obj"
 
 class Scenario
 {
@@ -19,11 +23,18 @@ private:
     
     std::string resourceFolder;
     
+    /* Textures */
+    GLubyte* floorTexture;
+    BITMAPINFO * floorTextureInfo;
+    GLuint floorTextureId;
+    
     void initTexture();
     
     void initMap();
     
     void initLight();
+    
+    void renderFloor();
     
 public:
 	C3DObject* map[SCENARIO_MAP_SIZE][SCENARIO_MAP_SIZE];
